b5e168ad293879e8bae6487fde03313f27c2ff00
emylet <mmanueljason@gmail.com>
2022-07-03 12:49:45 +0100

    never give up

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var adverChange = document.getElementById("advert-img");
 images = [
    'back-g/img-1.jpg','back-g/img-2.jpg',  'back-g/img-4.jpg',
@@ -11,6 +12,32 @@ setInterval(function() {
     adverChange.style.backgroundImage = 'url('+images[numb]+')'
 }, 2000);
  
+var srchBtn = document.getElementById("srchBtn");
+var adverImg = document.getElementById("advert-img");
+var resultDiv = document.getElementById("result-div");
+var vehicles = document.getElementById("advt-div");
+var errVal = document.getElementById("error-value");
+
+
+var myArr = ["hijet","toyota", "honda", "nissan", "mazda", "daihtsu", "suzuki"];
+
+function srchEl() {
+    var inputSrch = document.getElementById("input-srch").value;
+    inputSrch = inputSrch.toLowerCase();
+    var index = myArr.indexOf(inputSrch);
+
+        if(index !== -1) {
+            adverImg.style.display = "none"
+            resultDiv.style.display = "block"
+            resultDiv.innerHTML =inputSrch.toUpperCase() + "<strong>  is Aviliable</strong>" + "<br><br>"+"<a href='#'>click to view vehicle</a>"
+        } else{
+            adverImg.style.display = "none"
+            resultDiv.style.display = "block"
+            resultDiv.innerHTML = inputSrch.toUpperCase() + "<strong> not Aviliable</strong>" + "<br><br>"+"<a href='#'>click to view availiable vehicles</a>"
+        };
+};
+
+srchBtn.addEventListener("click",srchEl );
 
 
 
